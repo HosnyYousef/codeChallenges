88cd847217ca1e4e217e41383b3b984ca9ff7ffb
Yousef Hosny <YousefHosny.hm@gmail.com>
2022-05-18 13:11:11 -0700

    Delete occurrences of an element if it occurs more than n times

---
+++ CC 2022-05-18/app.js
@@ -0,0 +1,7 @@
+function deleteNth(arr,n){
+    const viewings = [1,2,3,1,2,1,2,3];
+    for (let index = 0; index < array.length; index++) {
+        const element = viewings[index];
+        if (ele > 2) break;
+    }
+  }
