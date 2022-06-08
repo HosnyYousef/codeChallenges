5f927ebcd34eaf74fd85491b3bce4539b0425eee
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-06-08 11:45:23 -0700

    Merge two sorted arrays into one | Codewars

--- June 2022/CC 2022-06-07/app.js
+++ June 2022/CC 2022-06-07/app.js
@@ -1,2 +1,38 @@
+function squareSum(numbers){
+
+}
+
+
 const squareSum = numbers => numbers.reduce((acc, curr) => acc + Math.pow(curr, 2), 0);
 
+
+
+function squareSum(numbers){
+    return numbers.map(x => x * x).reduce((prev,curr) => prev + curr, 0)
+    }
+
+    
+
+
+    function squareSum(numbers){
+        return numbers.reduce(function(sum, n){
+          return (n*n) + sum;
+        }, 0)
+      }
+
+
+
+
+      function squareSum(numbers){
+        return numbers.reduce((sum,num) => sum + (num * num), 0);
+      }
+
+
+
+      function squareSum(numbers){
+        var sum = 0;
+        numbers.forEach(function(n) {
+          sum += n * n
+        });
+        return sum;
+      }
---
+++ June 2022/CC 2022-06-08/app.js
@@ -0,0 +1,51 @@
+function mergeArrays(arr1, arr2) {
+    return Array.from(new Set(arr1.concat(arr2).sort((x,y) => x - y)))
+}
+
+    
+function mergeArrays(arr1, arr2) {
+    return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+}
+
+
+function mergeArrays(a, b) {
+    return [...new Set(a.concat(b))].sort((a,b)=>a-b)
+  }
+
+
+
+//my wrong solution:
+//   function mergeArrays(arr1, arr2) {
+//     const arr3 = arr1.concat(arr2)
+//     let unique = [...new Set(arr3)]
+//     return(unique)
+//   }
+  
+  
+  //combine arrays
+  //remove duplicate results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
