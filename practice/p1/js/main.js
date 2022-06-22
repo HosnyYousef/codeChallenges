8364d20adf03fd74bfda7b12fac1d456b4ed065e
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-06-22 10:04:17 -0700

    Remove First and Last Character Part Two

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,61 +1,252 @@
 function skipFives (n) {
-  for (let i = 1; i <= n; i++) {
-  if (n%5 !== 0){
-    console.log(i)
+    for (let i = 1; i <= n; i++) {
+    if (n%5 !==0){
+        console.log(i)
+    }        
+ }
+}
+skipFives(15, 2, 4, 6)
+
+
+function skipFives2 (n){
+    for (let i = 1; i <= n; i++) {
+    if (i%5 !==0){
+        console.log(i)
+     }   
     }
-  }
 }
 
-function hiBye (arr) {
-  if (arr[0] < arr[arr.length -1]){
-    alert ('Hi')
-  }else if (arr[0] > arr[arr.length -1]){
-    alert ('bye')
-  }else{
-    alert ('We close in an hour')
-  }
+function multiplyEachNumber (number){
+    let product = 1
+
+    let number.forEach (x => product * x){
+        alert (product)
+    }
+}
+multiplyEachNumber(15)
+
+
+
+
+function multiplyEachNumber2 (number) {
+    let product = 1
+
+    number.forEach (x => product *= x)
+    alert (product)
 }
 
-function multiplyNum (arr) {
-  let product = 1
-
-  arr.forEach(n => product *= n)
-    alert(product)
-}
-multiplyNum(2,4,6)
+multiplyEachNumber2 (15)
 
 
 function onlyEvens (arr) {
-  let evens = []
+    let evens = arr
 
-  for (let i = 0; i < arr.length; i++) {
-  if (i%2 === 0){
-    return evens
-    }
-  }
+    evens.forEach (x => {
+        if (x%2 === 0){
+            evens.push(x)
+        }
+    })
+    console.log ([1,2,3,4,5,6])
 }
+
 
 function onlyEvens2 (arr) {
-  let evens = []
+    let evens = []
 
-  arr.forEach (n => {
-    if (n%2 === 0){
-      return evens
+    evens.forEach (n => {
+        if (n%2 ===0){
+            evens.push(n)
+        }
+    })
+    return evens
+}
+console.log (onlyEvens([1,2,3,4,5,6]))
+
+
+
+
+
+
+function skipFives3 (n) {
+    for (let i = 1; i < n; i++) {
+    if (i%5 !== 0){
+        console.log(i)
+     }   
     }
-  })
-  evens (i)
+}
+skipFives3 (15, 20, 60)
+
+
+
+function skipFives4 (n) {
+    for (let i = 1; i <= n; i++) {
+    if (i%5 !== 0) { 
+        console.log(i)
+     }
+    }
+}
+skipFives4 (15)
+
+
+
+
+function positiveSum(arr) {
+  let postiveArr = arr.reduce (arr)
+
+  postiveArr.reduce ((previousValue, currentValue) => { /* ... */ } )
 }
 
 
-function onlyEvens3 (arr) {
-  let evens = []
-
-  arr.forEach(n => {
-    if (n%2 ===0){
-      arr.push (n)
-    }
-  })
-  return evens
+function positiveSum2 (arr) {
+    return arr.reduce((prev, current) => {
+        if (current > 0) {
+            return prev + current;
+        }
+    
+        return prev
+    }, 0)
 }
-console.log (arr([1,2,3,4,5,6]))
 
+function postiveSum (arr) {
+    let total = 0
+    for (let i = 0; i < array.length; i++) {
+    if (i>0){
+        return total
+     }
+        
+    }
+}
+
+function postiveSum (arr) {
+
+}
+
+
+function postiveSum (arr) {
+    
+}
+
+function postiveSum (arr) {
+    let total = 0
+    for (let i = 0; i < array.length; i++) {
+    if (arr[i]>0){
+        total += i
+    }
+        
+    }
+}
+
+
+
+function postiveSum (arr) {
+    let total = 0
+    for (let i = 0; i < array.length; i++) {
+    if (arr[i]> 0)
+        total += arr[i]
+        
+    }    
+return postiveSum
+}
+
+
+function postiveSum (arr) {
+    let total = 0
+    for (let i = 0; i < array.length; i++) {
+    if (arr[i]>0)
+        total += arr[i]
+    }   
+   return total
+}
+
+
+
+function postiveSum (arr) {
+    let sum = 0
+    
+    for (let i = 0; i < array.length; i++) {
+    if (arr[i]> 0){
+    total += arr[i]
+    }
+        
+    }
+return sum
+}
+
+
+function positiveSum4 (robot) {
+    let sum = 0
+
+    for (let i = 0; i < array.length; i++) {
+    if (robot[i]> 0){
+    sum += robot[i]
+        }
+    }
+return sum
+}
+
+function positiveSum5 (woofManBark) {
+    let woofSum = 0
+
+    for (let i = 0; i < array.length; i++) {
+    if (woofManBark[i]> 0){
+    woofSum += woofManBark[i]
+        }    
+    }
+return woofSum
+}
+
+
+let user = {}
+
+user.name = "John"
+user.surname = "Smith"
+
+user.name = "Pete"
+
+delete user.name
+
+
+let user = {}
+user.name = "John"
+user.surname = "Smith"
+user.name = "Pete"
+delete user.name
+
+let user = {}
+user.name = "John"
+user.surname = "Smith"
+user.name = "Pete"
+delete user.name
+
+
+
+
+let isEmpty = {}
+
+if (isEmpty = {}){
+    return ("true")
+}else{
+    return ("false")
+}
+
+
+let user = {
+    name: "John",
+    age: 30,
+    isAdmin: true
+  };
+  
+  for (let key in user) {
+    // keys
+    alert( key );  // name, age, isAdmin
+    // values for the keys
+    alert( user[key] ); // John, 30, true
+  }
+
+
+  function isEmpty2(obj) {
+      for (let key in obj) {
+          return false
+      }
+        return true
+  }
