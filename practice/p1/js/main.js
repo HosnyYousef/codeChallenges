b06c84dc6b788f0b6e5076db6cd9110601ccb7b2
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-06-29 12:09:34 -0700

    practice jun 29

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -6,3 +6,58 @@ function skippingFives (n){
   }
 }
 skippingFives (15)
+
+
+
+
+
+function onlyEvens (arr){
+  let evens = []
+
+  arr.forEach (x => {
+    if(x%2 ===0){
+      evens.push (x)
+    }
+  })
+  return evens
+}
+console.log (arr([1,2,3,4,5,6]))
+
+
+
+const duckWoofs = true
+alert (duckWoofs)
+
+
+let favColor = "red"
+favColor = "blue"
+console.log (favColor)
+
+
+
+function booleanAndString (x, string){
+  if (x (true)) {
+    alert (string)
+  }else if (x(false)){
+    console.log (string)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
