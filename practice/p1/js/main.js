d8f814c166b447cdb887425414d63d11bc4a7e66
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-06-29 10:36:41 -0700

    repeat of: sum of postive codewars challenge

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,71 +1,84 @@
-function skippingFives (n){
-  for (let i=1; i<n; i++) {
-  if (i%5 !==0) {
-    console.log(i)
-    }
-  }
-}
-
-skippingFives(15)
-
-
-
-
-function skippingFives2 (n){
+function skippingFives (n) {
   for (let i=1; i<=n; i++){
-  if (i%5 !==0){
+  if (i%5 !== 0){
     console.log (i)
     }
   }
 }
+skippingFives(15)
+
 
 
 function onlyEvens (arr) {
   let evens = []
 
-  arr.forEach (n => {
-    if (n%2 === 0){
-      evens.push (n)
+  for (let i=0; i<arr.length; i++) {
+  if (i%2 === 0) {
+    console.log (i)
     }
-  })
-  return evens
+    return evens
+  }
 }
-console.log (arr([1,2,3,4,5,6]))
+onlyEvens (arr([1,2,3,4,5,6]))
 
 
 function onlyEvens2 (arr) {
   let evens = []
 
   arr.forEach (n => {
-    if (n%2 === 0){
+    if (n%2 ===0){
       evens.push (n)
     }
+   
   })
-    return evens
+  return evens
 }
-
 console.log (onlyEvens2([1,2,3,4,5,6]))
 
+function onlyEvens3 (arr) {
+  let evens = []
 
-
-
-let woof = woof2
-woof = true
-
-
-
-
-
-let duck = true
-alert (duck)
-
-
-function positiveSum(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      sum += arr[i];
-      }
-  }
-  return sum;
+  arr.forEach (n => {
+    if (n%2===0){
+      evens.push(n)
+    }
+  })
+  return evens
 }
+console.log (onlyEvens3([1,2,3,4,5,6]))
+
+let woof = true
+alert (woof)
+
+let woofCat = red
+let red = blue
+console.log(woofcat)
+
+
+let favColor = 'red'
+favColor = 'blue'
+console.log (favColor)
+
+
+
+
+
+
+function sumOfThree (n1, n2, n3, n4) {
+  return (n1 + n2 + n3) / n4
+}
+sumOfThree (15, 20, 60, 2)
+
+
+function firstPowerToSecond (n1, n2) {
+  console.log (Math.pow(n1, n2))
+}
+firstPowerToSecond (20, 2)
+
+
+
+Syntax
+Math.pow(base, exponent)
+
+
+
