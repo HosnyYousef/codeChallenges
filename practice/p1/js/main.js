8db0821eba60785845b2998164ce7056392311aa
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-06-30 13:15:39 -0700

    practiced june 30th

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,13 +1,13 @@
 function skippingFives (n){
-  for (let i=0; i<=n; i++){
+
+  for (let i=1; i<=n; i++){
     if (i%5 !==0){
-      console.log (i)
+      console.log(i)
     }
   }
 }
+
 skippingFives (15)
-
-
 
 
 
@@ -15,49 +15,46 @@ function onlyEvens (arr){
   let evens = []
 
   arr.forEach (x => {
-    if(x%2 ===0){
-      evens.push (x)
+    if (x%2 === 0){
+      evens.push(x)
     }
   })
-  return evens
+    return evens
 }
-console.log (arr([1,2,3,4,5,6]))
+
+console.log (onlyEvens([1,2,3,4,5,6]))
 
 
 
-const duckWoofs = true
-alert (duckWoofs)
+let barkLikeADog = true
+alert (barkLikeADog)
 
-
-let favColor = "red"
-favColor = "blue"
+let favColor = 'red'
+favColor = 'blue'
 console.log (favColor)
 
 
-
-function booleanAndString (x, string){
-  if (x (true)) {
+function booleanAndString (b, string) {
+  if (b){
     alert (string)
-  }else if (x(false)){
+  }else{
     console.log (string)
   }
 }
 
 
+const booleanAndString2 = (b, string) => b ? alert(string) : console.log(string)
 
 
+function fizz (n){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  for (let i=1; i<n; i++){
+    if (n%3===0){
+      console.log ("fizz")
+    }else if (n%5 ===0){
+      console.log ("buzz")
+    }else if (n%3 === 0 && n%5 ===0){
+      console.log ("fizzbuzz")
+    }
+  }
+}
