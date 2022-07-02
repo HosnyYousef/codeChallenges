894a7192f38df26b7fd8b4e18dcca4a195ae1f9a
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-07-02 10:43:45 -0700

    practice

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,17 +1,4 @@
-function skippingFives (n){
-
-  for (let i=1; i<=n; i++){
-    if (i%5 !==0){
-      console.log(i)
-    }
-  }
-}
-
-skippingFives (15)
-
-
-
-function onlyEvens (arr){
+function onlyEvens (arr) {
   let evens = []
 
   arr.forEach (x => {
@@ -19,42 +6,83 @@ function onlyEvens (arr){
       evens.push(x)
     }
   })
-    return evens
+  return evens
 }
 
 console.log (onlyEvens([1,2,3,4,5,6]))
 
 
 
-let barkLikeADog = true
-alert (barkLikeADog)
+
+let woof = true
+alert (woof)
+
+
 
 let favColor = 'red'
 favColor = 'blue'
 console.log (favColor)
 
 
+
 function booleanAndString (b, string) {
   if (b){
-    alert (string)
+    alert(string)
   }else{
-    console.log (string)
+    console.log(string)
   }
 }
 
 
-const booleanAndString2 = (b, string) => b ? alert(string) : console.log(string)
+
+const booleanAndString2 = (b, string) => b ? alert(string): console.log(string)
 
 
-function fizz (n){
 
-  for (let i=1; i<n; i++){
-    if (n%3===0){
-      console.log ("fizz")
-    }else if (n%5 ===0){
-      console.log ("buzz")
-    }else if (n%3 === 0 && n%5 ===0){
-      console.log ("fizzbuzz")
+function fizzbuzz (n) {
+  for (let i=1; i<=n; i++){
+    if (i%3 ===0){
+      console.log('fizz')
+    }else if (i%5 ===0){
+      console.log('buzz')
+    }else if (i%3 === 0 && i%5=== 0){
+      console.log ('fizzbuzz')
+    }else{
+      return (i)
     }
   }
 }
+
+
+
+
+function fizzbuzz2 (n) {
+  for (let i=1; i<=n; i++){
+    if (i%3 === 0) {
+      console.log ('fizz')
+    }else if (i%5 === 0) {
+      console.log ('buzz')
+    }else if (i%3 === 0 && i%5 === 0){
+      console.log ('fizzbuzz')
+    }else{
+      console.log (i)
+    }
+  }
+}
+
+
+
+
+
+let movies = ['batman', 'spiderman', 'superman']
+
+//wrong:
+// movies[0] = woof
+
+let firstMovie = movies[0]
+
+let arrLength = movies.length 
+console.log (arrLength)
+
+
+
