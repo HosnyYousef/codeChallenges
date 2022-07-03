ff428ca3ee83b8e5f3f93b75eaf4cf7e08687f80
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-07-03 10:44:46 -0700

    practice classwork

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,13 +1,37 @@
-let sum = 0
-for (let key in salaries){
-  sum += salaries[key]
+function onlyEvens (arr){
+  let evens = []
+
+  arr.forEach (x => {
+    if (x%2 === 0){
+      evens.push(x)
+    }
+  })
+  return evens
 }
 
-alert (sum)
+console.log (onlyEvens([1,2,3,4,5,6]))
 
 
-function multiplyNumeric(obj) {
-  for (let key in obj) {
-    
+const booleanAndString = (b, string) => b ? alert(string): console.log(string)
+
+
+
+
+function fizzBuzz (n){
+  for (let i=1; i<=n; i++){
+    if (i%3 ===0){
+      console.log ('fizz')
+    }else if (i%5 ===0){
+      console.log ('buzz')
+    }else if (i%3 ===0 && i%5 ===0){
+      console.log ('fizzbuzz')
+    }else{
+      console.log (i)
+    }
   }
 }
+
+
+
+
+
