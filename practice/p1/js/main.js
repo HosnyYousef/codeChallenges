c66eae8296cbcbdf0dd0ecce9beebbd7886bac46
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-07-06 16:49:52 -0700

    practice class drills

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,4 +1,28 @@
-alert(message); // shows the variable content
+function onlyEvens (arr) {
+    let evens = []
+
+    arr.forEach (x => {
+        if (x%2 ===0){
+             evens.push(x)
+        }
+    })
+        return evens
+}
+console.log (onlyEvens([1,2,3,4,5,6]))
 
 
- 
+function fizzBuzz (n) {
+    for (let i=1; i <= n; i++){
+    if (i%3 === 0){
+        console.log ('buzz')
+        }else if (i%5 === 0){
+            console.log ('fizz')
+        }else if (i%3 === 0 && i%5 === 0){
+            console.log ('fizzbuzz')
+        }else{
+            console.log (i)
+        }
+    }
+}
+
+
