53ebab11c334a816ff09c8f14936d9cef5e98758
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-07-08 11:28:38 -0700

    practiced class drills

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,18 +1,5 @@
-let movies = ['fooman', 'batman', 'superman']
+let movies = ['woof', 'dog', 'cat']
 
-let firstMovie = movies[0]
-
-let movieLength = movies.length
-console.log (movieLength)
-
-let lastElement = movies [movies.length - 1]
-
-
-
-let MovieTitles = ['fooman', 'batman', 'superman']
-
-for (i = 0; i < MovieTitles.length; i++){
-    document.querySelector('h2').innerText += MovieTitles[i]
+for (let i=0; i < movies.length; i++){
+    document.querySelector('h2').innerText += movies [i]
 }
-
-let array of 
