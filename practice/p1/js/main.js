9bbe1e60a954ae2405f54efb1027cd3afb12ba8c
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-07-12 14:08:53 -0700

    hamburger menu practice

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,28 +1,10 @@
-// let user = {}
-// user.name = 'John'
-// user.surname = 'Smith'
-// user.name = 'John'
-// delete user.name
-
-
-// function isEmpty (obj){
-//     for (let key in obj){
-//         return false
-//     }
-//     return true
-// }
-
-
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-  }
-
-
-let sum = 0
-
-for (let key in salaries) {
-    sum += salaries[key]
+function toggleClass() {
+  let menu = document.querySelector(".mainMenu");
+  menu.classList.toggle("toggleCls")
 }
-alert(sum)
+
+//Same thing:
+
+// let hamburger = document.querySelector(".hamIcon");
+
+// hamburger.addEventListener("click", toggleClass);
