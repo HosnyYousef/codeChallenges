926e109271a3d6831641547fd3cbe653d8033dcb
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-07-14 11:36:58 -0700

    reading drills

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,27 +1,50 @@
+// 
 
-// function onlyEvens (arr) {
-//     let evens = []
 
-//     arr.forEach (x => {
-//         if (x%2 ===0){
-//             evens.push(x)
-//         }
-//     })
-//         return evens
+// function isEmpty (obj) {
+//     for (let key in obj){
+//         return false
+//     }
+//         return true
 // }
-// console.log(onlyEvens([1,2,3,4,5,6]))
 
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
 
+//   let sum = 0
+//   for (let key in salaries){
+//     sum += salaries[key]
+//   }
 
-let movies = ['dog', 'woof', 'barkLikeADog']
+//   console.log(sum)
 
-for (let i = 0; i < movies.length; i++){
-    document.querySelector('h2').innerText += movies[i]
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+function multiplyNumeric (obj) {
+    let total = 1
+    
+    for (let key in obj){
+        total = obj[key] *= 2
+    }
 }
 
-let num = [10, 20, 40]
-let sum = 0
+console.log (total)
 
-num.forEach ((item, x) => {
-    sum += item 
-})
+console.log(typeof 42);
+// expected output: "number"
+
+console.log(typeof 'blubber');
+// expected output: "string"
+
+console.log(typeof true);
+// expected output: "boolean"
+
+console.log(typeof undeclaredVariable);
+// expected output: "undefined"
