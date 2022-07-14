8eded1d2c8b7ff996fc527b1da4b47345eb44cf5
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2022-07-14 11:08:35 -0700

    practice class drills

--- practice/p1/js/main.js
+++ practice/p1/js/main.js
@@ -1,42 +1,27 @@
+
 // function onlyEvens (arr) {
 //     let evens = []
 
 //     arr.forEach (x => {
 //         if (x%2 ===0){
-//             evens.push (x)
+//             evens.push(x)
 //         }
 //     })
 //         return evens
 // }
+// console.log(onlyEvens([1,2,3,4,5,6]))
 
-// console.log (onlyEvens([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]))
 
 
-// function fizzBuzz (n) {
-//     for (let i=1; i<=n; i++){
-//         if (i%3 === 0 && i%5 === 0){
-//             console.log ('fizzbuzz')
-//         }else if (i%3 === 0){
-//             console.log ('fizz')
-//         }else if (i%5 === 0){
-//             console.log ('buzz')
-//         }else{
-//             console.log (i)
-//         }
-//     }
-// }
-// fizzBuzz (100)
+let movies = ['dog', 'woof', 'barkLikeADog']
 
-// let movies = ['woof', 'man', 'dog']
+for (let i = 0; i < movies.length; i++){
+    document.querySelector('h2').innerText += movies[i]
+}
 
-// for (let i=0; i<movies.length; i++){
-//     document.querySelector('h2').innerText += movies[i]
-// }
+let num = [10, 20, 40]
+let sum = 0
 
-let nums = [10, 20, 30]
-
-nums.forEach ((item, i) => {
-    nums[i] = item + 3
+num.forEach ((item, x) => {
+    sum += item 
 })
-
-console.log (nums)
