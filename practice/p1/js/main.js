8373440268bd827fff23fe774bf6616ba49b2e25
Yousef Hosny <yousefhosny.hm@gmail.com>
2022-07-21 10:24:17 -0700

    practice class drills

---
+++ practice/p1/js/main.js
@@ -0,0 +1,17 @@
+// let movies = ['woof', 'man', 'barks']
+// for (let i=0; i<movies.length; i++){
+//     document.querySelector('h2').innerText += movies[i]
+// }
+
+let nums = [20, 50, 60]
+
+nums.forEach ((item, i)=>{
+    nums[i] = item + 3
+})
+
+
+var total = 0;
+for(var i = 0; i < grades.length; i++) {
+    total += grades[i];
+}
+var avg = total / grades.length;
